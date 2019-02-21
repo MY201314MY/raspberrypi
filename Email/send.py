2b2636d3c6a9ce0ceef64be207a71041ec999053
mongoose <36258286+MY201314MY@users.noreply.github.com>
2019-02-21 22:47:31 +0800

    send an email to anohter mailbox
    Use 163 mailbox to Tencent mailbox.

---
+++ Email/send.py
@@ -0,0 +1,28 @@
+#coding: utf-8  
+import smtplib
+from email.mime.text import MIMEText
+
+print("******Welcome to site******")
+
+sender = '17634802048@163.com'
+receiver = '3477669308@qq.com'
+subject = 'python email test'
+host = 'smtp.163.com'
+port=25
+username = '17634802048@163.com'
+password = '411324ZM'
+
+message='<h1>Attention</h1><p>This is a message from pi.</p>The address is 192.168.3.6 %s from M</p>' % "thank you."
+msg = MIMEText(message, 'html', 'utf-8')
+msg['Subject'] = 'raspberrypi'
+msg['from'] = sender
+msg['to'] = receiver
+
+try:
+    smtp = smtplib.SMTP(host, port)
+    smtp.login(username, password )
+    smtp.sendmail( sender, receiver, msg.as_string())
+    print("Send to %s successfully" % receiver)
+    smtp.quit()
+except smtplib.SMTPException:
+        print("******Error!******")
